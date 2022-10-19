02cbcfba01f1edc4ff118b5686581b0cb84a3000
simon-1337 <simon.besenbaeck@gmail.com>
2022-10-19 15:23:28 +0200

    added links in nav_template.js

--- js/nav_template.js
+++ js/nav_template.js
@@ -24,28 +24,28 @@ function renderMainNavItems() {
 }
 
 function renderNavItemSummery() {
-    return `<a href="" class="nav-item nav-summary flex" id="1" onclick="">
+    return `<a href="summery.html" class="nav-item nav-summary flex" id="1" onclick="">
                 <img src="assets/img/summary-nav-icon.png">
                 <p>Summary</p>
             </a>`;
 }
 
 function renderNavItemBoard() {
-    return `<a href="" class="nav-item nav-board flex" id="2" onclick="">
+    return `<a href="board.html" class="nav-item nav-board flex" id="2" onclick="">
                 <img src="assets/img/board-nav-icon.png">
                 <p>Board</p>
             </a>`;
 }
 
 function renderNavItemAddTask() {
-    return `<a href="" class="nav-item nav-add-task flex" id="3" onclick="">
+    return `<a href="add_task.html" class="nav-item nav-add-task flex" id="3" onclick="">
                 <img src="assets/img/add-task-nav-icon.png">
                 <p>Add Task</p>
             </a>`;
 }
 
 function renderNavItemContacts() {
-    return ` <a href=" " class="nav-item nav-contacts flex" id="4" onclick="">
+    return ` <a href="contacts.html" class="nav-item nav-contacts flex" id="4" onclick="">
                 <img src="assets/img/contacts-nav-icon.png">
                 <p>Contacts</p>
             </a>`;
