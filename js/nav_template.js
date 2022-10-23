7a31a8f86b01de0091ea770437d6374084fa41e0
Kevin Schumilo <kevuschka@outlook.com>
2022-10-23 19:15:25 +0200

    resolve nav_temp

--- js/nav_template.js
+++ js/nav_template.js
@@ -8,10 +8,10 @@ function renderNavContent() {
     return `<img class="logo-img absolute" src="assets/img/logo-big.png">
             <div class="nav-selection flex column" id="nav-selection">
                 <div class="nav-main-selection flex column" id="nav-main-selection"></div>
-                <div class="nav-item nav-legal-notice flex" onclick="renderLegalnotice()">
+                <a href="legal_notice.html" class="nav-item nav-legal-notice flex">
                     <img src="assets/img/legal-notice.png">
                     <p>Legal notice</p>
-                </div>
+                </a>
             </div>`;
 }
 
@@ -24,28 +24,28 @@ function renderMainNavItems() {
 }
 
 function renderNavItemSummery() {
-    return `<a href="summary.html" class="nav-item nav-summary flex" id="1" onclick="">
+    return `<a href="summary.html" class="nav-item nav-summary flex" id="1">
                 <img src="assets/img/summary-nav-icon.png">
                 <p>Summary</p>
             </a>`;
 }
 
 function renderNavItemBoard() {
-    return `<a href="board.html" class="nav-item nav-board flex" id="2" onclick="">
+    return `<a href="board.html" class="nav-item nav-board flex" id="2">
                 <img src="assets/img/board-nav-icon.png">
                 <p>Board</p>
             </a>`;
 }
 
 function renderNavItemAddTask() {
-    return `<a href="add_task.html" class="nav-item nav-add-task flex" id="3" onclick="">
+    return `<a href="add_task.html" class="nav-item nav-add-task flex" id="3">
                 <img src="assets/img/add-task-nav-icon.png">
                 <p>Add Task</p>
             </a>`;
 }
 
 function renderNavItemContacts() {
-    return ` <a href="contacts.html" class="nav-item nav-contacts flex" id="4" onclick="">
+    return ` <a href="contacts.html" class="nav-item nav-contacts flex" id="4">
                 <img src="assets/img/contacts-nav-icon.png">
                 <p>Contacts</p>
             </a>`;
