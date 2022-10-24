c2a7d3411bdbb4501fe27818515054941a889f71
Kevin Schumilo <kevuschka@outlook.com>
2022-10-25 01:48:17 +0200

    some json alpha state

--- js/board.js
+++ js/board.js
@@ -1,91 +1,45 @@
-let todo = [{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-    },{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-    },{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
+let title;
+let description;
+let state;
+
+let category = [{
+    'name': 'design',
+    'color': '#FF7A00'
 }]
 
-let progress = [{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-    },{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-    },{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
+
+let contacts = [{
+    'name': 'Simon Meyer',
+    'color': '#0190E0',
+    'email': 'simon.meyer@gmail.com',
+    'phone': '+49 0123 456 78 9'
+},{
+    'name': 'Maximilian Vogel',
+    'color': '#EE00D6',
+    'email': 'maximilian.vogel@gmail.com',
+    'phone': '+49 0123 456 78 9'
+},{
+    'name': 'Eva Fischer',
+    'color': '#02CF2F',
+    'email': 'Eva.Fischer@gmail.com',
+    'phone': '+49 0123 456 78 9'
 }]
 
-let feedback = [{
+
+let task = [{
     'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-    },{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-    },{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
+    'title': 'Website redesign',
+    'description': "Modify the contents of the main website. Adjust the UI to the company's brand design.",
+    'process': '0/2',
+    'team': [],
+    'prior': 'assets/img/green.png',
+    'state':'todo'
 }]
 
-let done = [{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-    },{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-    },{
-    'category': category,
-    'title': title,
-    'description': description,
-    'process': process,
-    'contacts': [],
-    'state': state
-}]
+let todo = []
+
+let progress = []
+
+let feedback = []
+
+let done = []
