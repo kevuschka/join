1c6faf2f95694e767343d09a262c72bc2bb87ac9
simon-1337 <simon.besenbaeck@gmail.com>
2022-10-25 14:43:10 +0200

    copied html into a template function add_task.html

--- js/add_task.js
+++ js/add_task.js
@@ -3,8 +3,62 @@ let dropdownContacts = ['Hans', 'Jürgen']
 
 
 function renderAddTask() {
+    renderAddTaskContainer()
     renderCategoryDropdown();
     renderContactsDropdown();
+    renderPrio
+}
+
+
+function renderAddTaskContainer() {
+    let container = document.getElementById('content-container');
+    container.innerHTML = templateAddTask();
+}
+
+
+function templateAddTask() {
+    return /*html*/ `
+        <h1>Add Task</h1>
+        <form class="content-container-child flex">
+            <div class="add-task-column-left add-task-column flex column">
+                <div class="add-task-column-left-child flex column">
+                    <span class=>Title</span>
+                    <input type="text" id="title" class="add-task-input margin-bottom-24" placeholder="Enter a title">
+                </div>
+                <div class="add-task-column-left-child flex column">
+                    <span class=>Description</span>
+                    <textarea name="description" id="description" placeholder="Enter a description" class="add-task-textarea margin-bottom-24"></textarea>
+                </div>
+                <div class="add-task-column-left-child flex column">
+                    <span class=>Category</span>
+                    <div class="dropdown-container margin-bottom-24">
+                        <div class="dropdown" onclick="changeVisibilityDropdown('category-dropdown')">
+                            <span id="dropdown-text-category">Select task category</span>
+                            <img src="./assets/img/vector_2.png">
+                        </div>
+                        <div class="dropdown-content d-none" id="category-dropdown"></div>
+                    </div>
+                </div>
+                <div class="add-task-column-left-child flex column">
+                    <span class=>Assigned to</span>
+                    <div class="dropdown-container">
+                        <div class="dropdown" onclick="changeVisibilityDropdown('contacts-dropdown')">
+                            <span>Select contacts to assign</span>
+                            <img src="./assets/img/vector_2.png">
+                        </div>
+                        <div class="dropdown-content d-none" id="contacts-dropdown">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="add-task-column-right add-task-column">
+                <div class="add-task-column-right-child w-100 flex column">
+                    <span class=>Due Date</span>
+                    <input type="date" class="add-task-input margin-bottom-24" min="2022-10-01" max="2030-12-31">
+                </div>
+            </div>
+        </form>
+    `;
 }
 
 
