d42bc29882d5f72b26ff1b256cf8f4c54070c605
Kevin Schumilo <kevuschka@outlook.com>
2022-10-27 12:20:33 +0200

    added more to board js

--- js/board.js
+++ js/board.js
@@ -30,7 +30,8 @@ let task = [{
     'category': [],
     'title': 'Website redesign',
     'description': "Modify the contents of the main website. Adjust the UI to the company's brand design.",
-    'process': '0/2',
+    'process': 0,
+    'subtasks': 2,
     'team': [],
     'prior': 'assets/img/green.png',
     'board': 0
@@ -70,7 +71,9 @@ let progress = []
 let feedback = []
 
 let done = []
-
+let boardColumns = [todo, progress, feedback, done];
+let boardColumnTitle = ['To do', 'In progress', 'Awaiting Feedback', 'Done'];
+let emptyBoardColumn = ['No task to do', 'Nothing in progess', 'No Feedback awaiting', 'Nothing here'];
 
 function templateBoard() {
     renderResponsiveHeaderTitle();
@@ -96,13 +99,13 @@ function templateBoard() {
                                     </div>
                                 </div>
                             </div>
-                            <div class="board-content flex">
+                            <div class="board-content flex" id="board-content">
                                 <div class="board-column flex column">
                                     <div class="board-column-header flex cursor-p">
                                         <p>To do</p>
                                         <img class="board-column-header-plus" src="assets/img/plus-icon-big.png">
                                     </div>
-                                    <div class="board-tickets flex column" id="board_1">
+                                    <div class="board-tickets flex column" id="board-0">
                                         <div class="ticket-container flex column cursor-p">
                                             <div class="ticket-category-container flex">
                                                 <p class="ticket-category">Design</p>
@@ -134,23 +137,141 @@ function templateBoard() {
                                         <p>In progress</p>
                                         <img class="board-column-header-plus" src="assets/img/plus-icon-big.png">
                                     </div>
-                                    <div class="board-tickets flex column" id="board_2"></div>
+                                    <div class="board-tickets flex column" id="board-1"></div>
                                 </div>
                                 <div class="board-column flex column">
                                     <div class="board-column-header flex cursor-p">
                                         <p>Awaiting Feedback</p>
                                         <img class="board-column-header-plus" src="assets/img/plus-icon-big.png">
                                     </div>
-                                    <div class="board-tickets flex column" id="board_3"></div>
+                                    <div class="board-tickets flex column" id="board-2"></div>
                                 </div>
                                 <div class="board-column flex column">
                                     <div class="board-column-header flex cursor-p">
                                         <p>Done</p>
                                         <img class="board-column-header-plus" src="assets/img/plus-icon-big.png">
                                     </div>
-                                    <div class="board-tickets flex column" id="board_4"></div>
+                                    <div class="board-tickets flex column" id="board-3"></div>
                                 </div>
                             </div>
                         </div>
                     </div>`;
 }
+
+
+function renderBoardContent() {
+    let content = document.getElementById('board-content');
+    content = '';
+    for (let i = 0; i < boardColumns.length; i++) {
+        content.innerHTML += renderTemplateBoardColumn(i);
+        renderTickets(i);
+    }
+}
+
+
+function renderTemplateBoardColumn(i) {
+    return `<div class="board-column flex column">
+                <div class="board-column-header flex cursor-p">
+                    <p>${boardTitle[i]}</p>
+                    <img class="board-column-header-plus" src="assets/img/plus-icon-big.png">
+                </div>
+                <div class="board-tickets flex column" id="board-column-${i}"></div>
+            </div>`;
+}
+
+
+function renderBoardColumnContent(n) {
+    let content = documnet.getElementById(`board-column-${n}`);
+    content.innerHTML = '';
+    if(n > 0) renderTemplateOnholdTicketResponsive(content);
+    if(boardColumns[n].length > 0) {
+    for (let j = 0; j < boardColumns[n].length; j++) {
+        content.innerHTML += renderTemplateTicket(n,j);
+    }
+    } else content.innerHTML = `<p class="noTask cursor-d w-100 d-none">${boardColumnTitle[n]}</p>`;
+}
+
+
+function renderTemplateOnholdTicketTarget() {
+    let content;
+    for (let i = 1; i < boardColumns.length; i++) {
+        content = document.getElementById(`board-column-${i}`);
+        content.innerHTML += `<div class="onhold-container onhold-container-last w-100"></div>`;
+    }
+}
+
+function renderTemplateOnholdTicketResponsive(content) {
+    content.innerHTML += `<div class="onhold-container onhold-container-first w-100"></div>`;
+}
+
+
+function pressAction() {
+    if(hold) {
+
+    }
+}
+
+
+// function renderTickets(i) {
+//     renderTemplateTickets(i);
+//     let boardColumnContent = document.getElementById(`board-column-${i}`);
+//     for (let index = 0; i < boardColumns.length; i++) {
+//         for (let j = 0; j < boardColumns[i].length; j++) {
+            
+            
+//             boardColumnContent += boardColumns[i][j];
+            
+//         } boardColumns[i].length
+        
+//     }
+// }
+
+function renderTemplateTickets(i) {
+    let content = documnet.getElementById(`board-column-${i}`)
+        if(boardColumns[i].length > 0) {
+            for (let j = 0; j < boardColumns[i].length; j++) {
+                content.innerHTML += renderTemplateTicket(i,j);
+            }
+        }        
+}
+
+function renderTemplateTicket(i,j) {
+    return `<div class="ticket-container flex column cursor-p">
+                <div class="ticket-category-container flex">
+                    <p class="ticket-category">${boardColumns[i][j]['category']}</p>
+                </div>
+                <div class="ticket-description-container flex column">
+                    <p class="ticket-description-title">${boardColumns[i][j]['title']}</p>
+                    <div class="ticket-description">
+                        ${boardColumns[i][j]['description']}
+                    </div>
+                </div>
+                <div class="process-bar-container flex">
+                    <div class="process-bar"></div>
+                    <div class="process-state">0/2 Done</div>
+                </div>
+                <div class="ticket-footer-container flex">
+                    <div class="ticket-contacts-container flex">
+                        <div class="ticket-contact" id="board-contact-0">SM</div>
+                        <div class="ticket-contact" id="board-contact-1">MV</div>
+                        <div class="ticket-contact" id="board-contact-2">EF</div>
+                    </div>
+                    <img class="state-img" src="assets/img/green.png">
+                </div>
+            </div>`;
+}
+
+function renderTicketCategory() {
+
+}
+
+// let task = [{
+//     'category': '',
+//     'title': 'Website redesign',
+//     'description': "Modify the contents of the main website. Adjust the UI to the company's brand design.",
+//     'process': 0,
+//     'subtasks': 2,
+//     'team': [],
+//     'prior': 'assets/img/green.png',
+//     'board': 0
+// }]
