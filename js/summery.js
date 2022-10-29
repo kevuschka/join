51e560248c52c6613e02c16a976fe311bfc6d192
Sezgin Gündogdu <sgbewerbung@yahoo.de>
2022-10-29 05:25:36 +0200

    updated summary-HTML/CSS/JS + responsive and updated login and signup CSS

---
+++ js/summery.js
@@ -0,0 +1,19 @@
+/**
+ * This function renders informations about the number of tasks in the categories
+ * 
+ * 
+ */
+function renderSummary() {
+    document.getElementById('task-count1').innerHTML = `${todo.length}`;
+    document.getElementById('task-count2').innerHTML = `${progress.length}`;
+    document.getElementById('task-count3').innerHTML = `${feedback.length}`;
+
+    document.getElementById('task-info-count').innerHTML = `${todo.length}`;
+    document.getElementById('task-info-count2').innerHTML = `${done.length}`;
+
+    
+
+    if (task[0]['prior']=='assets/img/green.png') {
+        document.getElementById('priority-Icon').src = 'assets/img/summary-arrow-up-icon.png';
+    }      
+}
