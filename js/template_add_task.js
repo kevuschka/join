8a0d61ce1b63d0147a8659cb3d87d57657f2a077
simon-1337 <simon.besenbaeck@gmail.com>
2022-11-02 16:14:41 +0100

    now using task_form.html and template_add_task.js to include the static content

---
+++ js/template_add_task.js
@@ -0,0 +1,19 @@
+async function initAddTask() {
+    await renderResponsiveHeaderTitle(); //in script.js
+    await includeHTML()
+    renderAddTask();  //in add_task.js
+}
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "assets/templates/task_form.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML += await resp.text();
+        } else {
+            element.innerHTML += 'Page not found';
+        }
+    }
+}
