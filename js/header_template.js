fc1c21631867bb8645fd30938fa1006ece67fce2
Kevin Schumilo <kevuschka@outlook.com>
2022-11-08 14:54:35 +0100

    add addTask resp-btn header

--- js/header_template.js
+++ js/header_template.js
@@ -8,9 +8,28 @@ function renderHeader() {
             <img class="logo-img-resp absolute d-none" src="assets/img/logo-big2.png">
         </div>
         <div class="header-right-corner-add-task d-none flex" id="header-right-corner-add-task">
-            <div class="header-create-btn flex">
+            <img class="logo-img-resp absolute d-none" src="assets/img/logo-big2.png">
+            <div class="header-create-btn cursor-p h-100 flex">
                 <p>Create</p>
                 <img src="assets/img/check-small.png">
             </div>
         </div>`;
+    whenItsAddtask();
+}
+
+function whenItsAddtask() {
+    if(window.location.pathname.includes('add_task.html') && (window.innerWidth < 801)) {
+        document.getElementById('header-right-corner').classList.add('d-none');
+        document.getElementById('header-right-corner-add-task').classList.remove('d-none');
+    }
+}
+
+window.onresize = function() {
+    if(window.location.pathname.includes('add_task.html') && (window.innerWidth < 801)) {
+        document.getElementById('header-right-corner').classList.add('d-none');
+        document.getElementById('header-right-corner-add-task').classList.remove('d-none');
+    } else {
+        document.getElementById('header-right-corner').classList.remove('d-none');
+        document.getElementById('header-right-corner-add-task').classList.add('d-none');
+    }
 }
