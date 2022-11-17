43d88efba64b296efec745ad3b3f37b016486216
Kevin Schumilo <kevuschka@outlook.com>
2022-11-17 20:55:30 +0100

    details in contacts

---
+++ js/contacts.js
@@ -0,0 +1,10 @@
+function renderContactsList() {
+    let list = document.getElementById(`contacts-list`);
+    filterContacts();
+}
+
+function filterContacts() {
+    for (let i = 0; i < alphabet.length; i++) {
+        let letter = moin;
+    }
+}
